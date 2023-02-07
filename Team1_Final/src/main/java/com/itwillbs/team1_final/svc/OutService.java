ba79844a641aa00dc628f26d3dbf1d0b94c4cc41
user <user@DESKTOP-2PGRMF4>
2023-02-07 18:28:46 +0900

    컴펌해주세요 !

--- Team1_Final/src/main/java/com/itwillbs/team1_final/svc/OutService.java
+++ Team1_Final/src/main/java/com/itwillbs/team1_final/svc/OutService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.itwillbs.team1_final.mapper.OutMapper;
 import com.itwillbs.team1_final.vo.AccVO;
 import com.itwillbs.team1_final.vo.HrVO;
+import com.itwillbs.team1_final.vo.OutListInfoVO;
 import com.itwillbs.team1_final.vo.OutPdVO;
 import com.itwillbs.team1_final.vo.OutSchVo;
 import com.itwillbs.team1_final.vo.PdVO;
@@ -85,10 +86,14 @@ public class OutService {
 			outSch.get(i).setEMP_NAME(emp_name);
 
 			// 품목 정보 조회
-			// OUT_SCHEDULE	->	OUT_SCHEDULE	->	product
-
-			
-			
+			String sOut_schedule_cd = outSch.get(i).getOUT_SCHEDULE_CD();
+			for(int j = 0; j < outSch.size(); j++) {
+				List<OutListInfoVO> outListInfo = mapper.selectoutListInfo(sOut_schedule_cd);
+				
+				outSch.get(i).setPRODUCT_NAME(outListInfo.get(j).getPRODUCT_NAME());
+				outSch.get(i).setSIZE_DES(outListInfo.get(j).getSIZE_DES());
+				
+			}
 			
 		}
 		return outSch;
